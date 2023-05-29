757038259c2e9f5751dc3c612882c57fc5cf7fba
GLCVLL <g.ioele96@outlook.it>
2023-05-29 19:28:23 +0200

    fixed data from user

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
 console.log('JS OK');
 
+const inputN = document.getElementById('name');
+console.log(inputN);
+const inputK = document.getElementById('kms');
+console.log(inputK);
+const inputA = document.getElementById('age');
+console.log(inputA);
+const buttonS = document.getElementById('send');
+console.log(buttonS);
+const buttonC = document.getElementById('cancel');
+console.log(buttonC);
+
+// ADD KM PRICE
+
+let kmPrice = 0.21;
+
+
+// GET DATA FROM USER
+
+buttonS.addEventListener('click', function(){
+    // RECUPERO VALORE
+    const name = inputN.value;
+    const kms = inputK.value;
+    const age = inputA.value;
+})
