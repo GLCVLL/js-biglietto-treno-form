fe3df1a549bafdd51d99f79f4765b4708546d5c0
GLCVLL <g.ioele96@outlook.it>
2023-05-30 09:07:21 +0200

    added display flex to my-ticket js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,25 @@ console.log('JS OK');
 
 const inputN = document.getElementById('name');
 console.log(inputN);
+
 const inputK = document.getElementById('kms');
 console.log(inputK);
+
 const inputA = document.getElementById('age');
 console.log(inputA);
+
 const buttonS = document.getElementById('send');
 console.log(buttonS);
+
 const buttonC = document.getElementById('cancel');
 console.log(buttonC);
+
 const paragraph = document.getElementById('name-message');
 console.log(paragraph);
+
+const myTicket = document.querySelector('.my-ticket');
+console.log(myTicket);
+
 
 // ADD KM PRICE
 
@@ -30,23 +39,18 @@ buttonS.addEventListener('click', function(){
     const kms = inputK.value;
     const age = inputA.value;
 
-    
+    //CHANGE DISPLAY
+
+    myTicket.style.display = 'flex';
 
 // calculating ticket price based on km
 
     let price = kmPrice * kms;
     console.log(price);
 
-
     // calculating discount
 
     if(age >= 65) discount = 40;
     else if(age <= 18) discount = 20;
-
-    if (discount){
-        const element = document.getElementById('ticket-price');
-        price -= (price / 100) * discount;
-        element.innerText = price.toFixed(2);
-    }
-})
+});
 
